6f7577eece2f987c4ef382ebc7ba2a3465d027ae
aryahdyt <aryapakehidayat@gmail.com>
2021-08-23 18:50:52 +0700

    first commit

---
+++ database/factories/JurusanFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Jurusan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JurusanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Jurusan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('template.index');
+// });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
